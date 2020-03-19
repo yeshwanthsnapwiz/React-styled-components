c43c4226954e0707907336e17040f33fdb8752b4
yeshwanth.R <58871940+yeshwanthgithub@users.noreply.github.com>
2020-03-19 14:03:01 +0530

    updated form
    added styling to input

--- materialkit/src/form/form.js
+++ materialkit/src/form/form.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import styled from 'styled-components';
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from  '@fortawesome/free-brands-svg-icons';
-import {  faFacebookSquare } from  '@fortawesome/free-brands-svg-icons';
-import {  faGooglePlusG } from  '@fortawesome/free-brands-svg-icons';
-import { faUser } from  '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from  '@fortawesome/free-solid-svg-icons';
-import { faLock } from  '@fortawesome/free-solid-svg-icons';
+import { faFacebookSquare } from  '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from  '@fortawesome/free-brands-svg-icons';
+import {faUser } from  '@fortawesome/free-solid-svg-icons';
+import {faEnvelope } from  '@fortawesome/free-solid-svg-icons';
+import {faLock} from  '@fortawesome/free-solid-svg-icons';
+import {Formwrapper} from './formcss';
+import {HEAD} from './formcss';
+import {TITLE,SOCIALLOGO,LI,Span,FORMSECTION,SECOND,SECONDINPUT,Userlabelb,Nameuser,A} from './formcss';
+
 
 
 export const Form = ({children,className}) => {
@@ -18,7 +21,7 @@ export const Form = ({children,className}) => {
                   LOGIN
               </TITLE>
               <SOCIALLOGO>
-              <LI as="a" href="#"><FontAwesomeIcon className="twittera"  icon= { faTwitter } / ></LI>
+              <LI as="a" href="#"><FontAwesomeIcon   className="twittera"  icon= { faTwitter } / ></LI>
         <LI as="a" href="#"><FontAwesomeIcon className="facebookb"  icon= {  faFacebookSquare }/></LI>
         <LI as="a" href="#"><FontAwesomeIcon className="GooglePlus"    icon= {  faGooglePlusG  }/></LI>
               </SOCIALLOGO>
@@ -26,18 +29,36 @@ export const Form = ({children,className}) => {
           <Span>Or Be Classical</Span>
       
           <FORMSECTION>
-        <FORMGROUP>
-        <FontAwesomeIcon className="user"  icon= { faUser }/ >
-        <Input type="text" placeholder="First Name..." />
-        </FORMGROUP>
-        <FORMGROUP>
-        <FontAwesomeIcon className="email"  icon= { faEnvelope  }/ >
-        <Input type="email" placeholder="Email..." />
-        </FORMGROUP>
-        <FORMGROUP>
-        <FontAwesomeIcon className="password"  icon= { faLock }/ >
-        <Input type="password" placeholder="Password..." />
-        </FORMGROUP>
+      
+        <SECOND className ="usernameb">
+        <FontAwesomeIcon  className="user"  icon= { faUser }/ >
+        <SECONDINPUT type="text" className="first"  name="fname"  />        
+        <Userlabelb className="userlabel" for="fname">
+        <Nameuser className="nameb">Username...</Nameuser>
+         </Userlabelb>
+          </SECOND>
+     
+      
+          <SECOND className ="usernameb">
+          <FontAwesomeIcon className="email"  icon= { faEnvelope  }/ >
+        <SECONDINPUT type="text" className="first"  name="fname"  />        
+        <Userlabelb className="userlabel" for="fname">
+        <Nameuser className="nameb">Email...</Nameuser>
+         </Userlabelb>
+          </SECOND>
+
+      
+      
+          <SECOND className ="usernameb">
+          <FontAwesomeIcon className="password"  icon= { faLock }/ >
+        <SECONDINPUT type="text" className="first"  name="fname"  />        
+        <Userlabelb className="userlabel" for="fname">
+        <Nameuser className="nameb">Password...</Nameuser>
+         </Userlabelb>
+          </SECOND>
+
+       
+       
          
           </FORMSECTION>
           <A href="#">GET STARTED</A>
@@ -48,113 +69,5 @@ export const Form = ({children,className}) => {
     )
 }
 
-
-const Formwrapper = styled.div`
-display:flex;
-flex-direction:column;
-align-items:center;
-max-width:350px;
-min-height:420px;
-background-color:white;
-margin:auto;
-position:relative;
-top:80px;
-border-radius:5px;
-
-
-`;
-;
-const HEAD = styled.div`
-display:flex;
-flex-direction:column;
-  min-width:305px;
-  min-height:150px;
-  border-radius:3px;
- background-image: linear-gradient(90deg,#a84fb9,#8343a5);
-  position:relative;
-  top:-45px;
-  box-shadow: 0px 10px 10px 10px rgba(199,172,206,0.3);
-
-`;
- 
-const TITLE =styled.div`
-color:white;
-align-self:center;
-height:60px;
-position:relative;
-top:30px;
-font-family:Roboto Slab,Times New Roman,serif;
-font-size:16px;
-font-weight:700;
-
-`;
-const SOCIALLOGO =styled.div`
-
-display:flex;
-justify-content:center;
-justify-content:space-around;
-position:relative;
-top:30px;
-.twittera {
-    position:relative;
-    left:50px;
-}
-.GooglePlus {
-    position:relative;
-    right:50px;
-}
-`;
-const LI =styled.li`
-list-style-type:none;
-color:white;
-font-size:18px;
-text-decoration:none;
-`;
-const FORMSECTION =styled.form`
-width:100%;
-
-`;
-
-const FORMGROUP = styled.li`
-list-style-type:none;
-padding-top:10px;
-
-.user, .email, .password
-{
-  
-  
-    font-size:23px;
-    color:grey;
-    min-width:20%;
-    height:22px;
-
-}
-`;
-const Span =styled.span`
-color:grey;
-font-size:15px;
-font-weight:50;
-position:relative;
-top:-28px;
-`;
-const Input = styled.input`
-width:70%;
-height:50px;
-border:none;
-border-bottom:1px solid lightgrey;
-    &::placeholder{
-        position:relative;
-        top:-1px;
-        font-size:15px;
-        color:grey;
-
-    }
-`;
-const A =styled.a`
-
-text-decoration:none;
-position:relative;
-top:30px;
-`;
 
 export default Form;
---
+++ materialkit/src/form/formcss.js
@@ -0,0 +1,243 @@
+
+
+import styled from 'styled-components';
+
+export const label =`
+position:absolute;
+bottom:8px;
+left:50px;
+transition:all 0.3 ease;
+`;
+
+
+export const name=`
+position:absolute;
+bottom:15px;
+left:0px;
+transition:all 0.5 ease;
+font-size:17px;
+
+`;
+export const inputfocus =`
+outline:none;
+position:relative;
+`;
+
+export const onfocuslabel =`
+font-size:12px;
+color:#7a00ff;
+`;
+export const onfocuslabelafter =`
+transform:X(0%);
+z-index:1;
+transform: scaleX(122);
+@media only screen and (max-width: 680px) {
+  transform: scaleX(125);
+
+   
+ }
+`;
+
+export const SECOND = styled.div`
+
+width:350px;
+height:60px;
+position: relative;
+overflow:hidden;
+.user , .email, .password{
+      position: relative;
+  left: 10%;
+  top:30%;
+  color:grey;
+}
+@media only screen and (max-width: 680px) {
+    margin:0px 0px 0px 0px;
+ 
+ 
+}
+.nameb {
+${name};
+
+
+}
+  .first:focus{
+    ${inputfocus};
+   
+  }
+  .first:focus + .userlabel .nameb
+  {
+   transform:translateY(-80%);
+  ${onfocuslabel};
+   
+  }
+  .first:focus + .userlabel::after , .form input:valid + .nameb::after  {
+    ${onfocuslabelafter};
+ 
+   }
+
+`;
+
+
+
+export const SECONDINPUT = styled.input`
+
+width:70%;
+ position: relative;
+ left:20%;
+ top:20px;
+ padding-top:10px;
+border:none;
+@media only screen and (max-width: 680px) {
+  min-width:100%;
+  margin:0px 0px 0px 0px;
+ 
+}`;
+
+
+export const Userlabelb = styled.label`
+position:absolute;
+  color:grey;
+  bottom:4px;
+  left:20%;
+  width:70%;
+  height:100%;
+  pointer-events:none;
+  border-bottom:1px solid lightgrey;
+  &::after {
+  content:"";
+  position:absolute;
+  left:60%;
+  bottom:-1px;
+  height:100%;
+  width:1%;
+  border-bottom:2px solid  #7a00ff;
+  z-index:-1;
+  transition:transform 0.5s ease;
+  @media only screen and (max-width: 680px) {
+   
+    position:absolute;
+  left:62%;
+  
+
+}
+}
+@media only screen and (max-width: 680px) {
+   
+    margin:0px 0px 0px 0px;
+   
+
+}
+`;
+
+
+export const Nameuser = styled.span`
+  ${label};
+`;
+
+export const Formwrapper = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+max-width:350px;
+min-height:420px;
+background-color:white;
+margin:auto;
+position:relative;
+top:80px;
+border-radius:5px;
+
+
+`;
+;
+export const HEAD = styled.div`
+display:flex;
+flex-direction:column;
+  min-width:305px;
+  min-height:150px;
+  border-radius:3px;
+ background-image: linear-gradient(90deg,#a84fb9,#8343a5);
+  position:relative;
+  top:-45px;
+  box-shadow: 0px 10px 10px 10px rgba(199,172,206,0.3);
+
+`;
+ 
+export const TITLE =styled.div`
+color:white;
+align-self:center;
+height:60px;
+position:relative;
+top:30px;
+font-family:Roboto Slab,Times New Roman,serif;
+font-size:16px;
+font-weight:700;
+
+`;
+export const SOCIALLOGO =styled.div`
+
+display:flex;
+justify-content:center;
+justify-content:space-around;
+position:relative;
+top:30px;
+.twittera {
+    position:relative;
+    left:50px;
+}
+.GooglePlus {
+    position:relative;
+    right:50px;
+}
+`;
+export const LI =styled.li`
+list-style-type:none;
+color:white;
+font-size:18px;
+text-decoration:none;
+`;
+export const FORMSECTION =styled.form`
+width:100%;
+
+`;
+
+export const FORMGROUP = styled.li`
+list-style-type:none;
+padding-top:10px;
+
+.user, .email, .password
+{
+  
+  
+    font-size:23px;
+    color:grey;
+    min-width:20%;
+    height:22px;
+
+}
+`;
+export const Span =styled.span`
+color:grey;
+font-size:15px;
+font-weight:50;
+position:relative;
+top:-28px;
+`;
+export const Input = styled.input`
+width:70%;
+height:50px;
+border:none;
+border-bottom:1px solid lightgrey;
+    &::placeholder{
+        position:relative;
+        top:-1px;
+        font-size:15px;
+        color:grey;
+
+    }
+`;
+export const A =styled.a`
+
+text-decoration:none;
+position:relative;
+top:30px;
+`;
